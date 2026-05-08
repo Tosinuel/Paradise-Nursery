261b317060f9304ede1d14c12f53e278dd3061cb
Emmanuel Oluwatosin <110412246+Tosinuel@users.noreply.github.com>
2026-05-08 15:41:27 +0100

    Update landing page heading text

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -4,7 +4,7 @@ import './LandingPage.css';
 export default function LandingPage() {
   return (
     <div className="landing fade-in">
-      <h1 className="slide-up">🌿 Paradise Nursery</h1>
+      <h1 className="slide-up">🌿 Welcome to Paradise Nursery </h1>
       <p className="slide-up">Where Green Meets Serenity. Explore our wide range of houseplants for a healthier lifestyle.</p>
       <Link to="/products">
         <button className="get-started slide-up">Get Started</button>
